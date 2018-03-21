061a6955e0f95bb3b2fbc2093ac3d4351808c8b6
Kleyson <kleysonb@hotmail.com>
2018-03-20 21:43:54 -0300

    Configurações de Banco e Templates

---
+++ myenv/myproject/myproject/urls.py
@@ -0,0 +1,8 @@
+
+from django.urls import include, path
+from django.contrib import admin
+
+urlpatterns = [
+    path('veiculos/', include('veiculos.urls')),
+    path('admin/', admin.site.urls),
+]
