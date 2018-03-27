8223297c476ad407f09e0d520fa9520c2426224f
Kleyson <kleysonb@hotmail.com>
2018-03-27 20:56:05 -0300

    crud implementado

--- myenv/myproject/veiculos/urls.py
+++ myenv/myproject/veiculos/urls.py
@@ -1,7 +1,10 @@
 
 from django.urls import path
-from . import views
+from.import views
 
 urlpatterns = [
     path('', views.VeiculosList.as_view(), name='listar-veiculos'),
+    path(r'novo/', views.VeiculosNew.as_view(), name='novo-veiculo'),
+    path(r'<int:pk>/', views.VeiculosEdit.as_view(), name='editar-veiculo'),
+    path(r'excluir/<int:pk>/', views.VeiculosDelete.as_view(), name='deletar-veiculo')
 ]
--- myenv/myproject/veiculos/views.py
+++ myenv/myproject/veiculos/views.py
@@ -1,15 +1,38 @@
-from django.views.generic import View
-from django.shortcuts import render
-from veiculos.models import Veiculo
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView
+from veiculos.forms import *
+from veiculos.models import *
 
-class VeiculosList(View):
+class VeiculosList(ListView):
 	"""
 	View para listar veiculos cadastrados.
 	"""
-	
-	def get(self, request):
-		context = {
-			'object_list': Veiculo.objects.all().order_by('marca'),
-			'total': Veiculo.objects.count()
-		}
-		return render(request, 'veiculos/listar.html', context)
+	model = Veiculo
+	template_name = 'veiculos/listar.html'
+
+
+class VeiculosNew(CreateView):
+	"""
+	View para criação de novos veiculos
+	"""
+	model = Veiculo
+	form_class = FormularioVeiculo
+	template_name = 'veiculos/novo.html'
+	success_url = '/veiculos'
+
+
+class VeiculosEdit(UpdateView):
+	"""
+	View para a edição de veiculos já cadastrados.
+	"""
+	model = Veiculo
+	form_class = FormularioVeiculo
+	template_name = 'veiculos/editar.html'
+	success_url = '/veiculos'
+
+class VeiculosDelete(DeleteView):
+	"""
+	View para a exclusão de veiculos.
+	"""
+	model = Veiculo
+	template_name = 'veiculos/excluir.html'
+	success_url = '/veiculos'
